666537ac7f74cb279bd7d2fb988ad20812a240cc
fahedarshad01 <fahed.arshad@ovo.com>
2024-10-30 18:19:00 +0000

    add some minor styling changes

--- kinwell-form/src/app/page.tsx
+++ kinwell-form/src/app/page.tsx
@@ -95,6 +95,21 @@ export default function Home() {
                 <b>Click Here to Start Consultation</b>
               </Button>
             </Grid>
+            <Grid
+              size={{ xs: 12 }}
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+            >
+              <Box
+                component="img"
+                src="https://imgur.com/Sp48G3I.png"
+                alt="NHS Scotland logo"
+                width={150}
+                sx={{ [theme.breakpoints.down("sm")]: { width: 150 } }}
+              />
+              <Typography textAlign="center">Providing NHS services</Typography>
+            </Grid>
           </Grid>
         </form>
       </Fade>
