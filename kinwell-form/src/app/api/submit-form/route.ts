6c0907fa5a455dd37e932f55e9791b8e327d3d80
fahedarshad01 <fahed.arshad@ovo.com>
2024-10-31 00:29:28 +0000

    change channel to kiosk for form

--- kinwell-form/src/app/api/submit-form/route.ts
+++ kinwell-form/src/app/api/submit-form/route.ts
@@ -60,7 +60,7 @@ export async function POST(req: NextRequest) {
     form.append("dateOfBirth", dateOfBirth);
     form.append("sex", sex);
     form.append("submittedAt", new Date().toISOString());
-    form.append("channel", "In Store");
+    form.append("channel", "Kiosk");
     form.append("pharmacyName", "KinWell Pharmacy");
 
     const response = await pharmaPalInstance.post("/chat/form", form, {
