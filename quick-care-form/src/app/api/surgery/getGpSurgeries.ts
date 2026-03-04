8df1fd6ceeb79cf146b06b69b9be2fd127c68fb7
Fahed Arshad <arshad.fahed@gmail.com>
2026-03-04 12:03:51 +0000

    add API key header to surgery endpoint request

--- quick-care-form/src/app/api/surgery/getGpSurgeries.ts
+++ quick-care-form/src/app/api/surgery/getGpSurgeries.ts
@@ -2,7 +2,13 @@ import { pharmaPalInstance } from "../lib/pharma-bal-backend.instance";
 
 export const getGpSurgeries = async () => {
   try {
-    return (await pharmaPalInstance("/surgery")).data;
+    return (
+      await pharmaPalInstance("/surgery", {
+        headers: {
+          "x-api-key": process.env.QUICK_CARE_API_KEY,
+        },
+      })
+    ).data;
   } catch (error) {
     return [];
   }
